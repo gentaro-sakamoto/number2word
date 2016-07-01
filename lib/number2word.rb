13a0b70157051a8de8016ba52c1b07ec2929a13c
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2016-07-01 09:50:27 +0900

    Allow to convert number to word.

--- lib/number2word.rb
+++ lib/number2word.rb
@@ -1,8 +1,28 @@
+INITIALIZER = -1
+require File.expand_path("../data/big_numbers.rb",File.dirname(__FILE__))
+require File.expand_path("../data/sub_one_thousand.rb",File.dirname(__FILE__))
 module Number2Word
   def convert(num)
+    negative = false
     valid_num = validate(num)
     return 'zero' if valid_num.zero?
-    puts valid_num
+    if valid_num < 0
+      valid_num = valid_num.abs
+      negative = true
+    end
+
+    processing_num = valid_num
+    stack = []
+    i = 0
+    until processing_num.zero?
+      word = ''
+      processing_num, remainder = processing_num.divmod(1000)
+      word << BIG_NUMBERS[i] if processing_num.nonzero? && processing_num != 1000
+      word << (word.empty? ? '' : ' ') + "#{SUB_ONE_THOUSAND[remainder]}" unless remainder.zero?
+      stack << word
+      i = i.succ
+    end
+    words = (negative ? 'negative ' : '') + stack.reverse.join(' ')
   end
 
   def validate(num)
