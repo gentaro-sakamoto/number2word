71b5cc3ad906afb26d3dc3bb6d04c2d4efb8ccf1
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2016-07-02 09:49:08 +0900

    Print converted number.

--- bin/number2word.rb
+++ bin/number2word.rb
@@ -4,4 +4,4 @@ lib = File.expand_path("../../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'number2word'
 include Number2Word
-convert(ARGV[0])
+puts convert(ARGV[0])
