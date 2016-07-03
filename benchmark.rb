00f1ea709e00327a34d2d95655c9cb4d66a49521
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2016-07-03 16:48:14 +0900

    Allow to benchmark converting float.

--- benchmark.rb
+++ benchmark.rb
@@ -5,4 +5,5 @@ include Number2Word
 
 Benchmark.benchmark do |x|
   x.report(:bin_num) { 100.times { |n| convert(rand(10**303..10**304)) } }
+  x.report(:float) { 1000.times { |n| convert(rand) } }
 end
